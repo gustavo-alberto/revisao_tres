1379d03c91fea66847ef0f9238ecb98f42fcd24e
Gustavo Alberto <83519205+gustavo-alberto@users.noreply.github.com>
2024-04-04 20:37:45 -0300

    feat: Resolve questão 4

--- array_e_indice.py
+++ array_e_indice.py
@@ -36,3 +36,27 @@ def print_second_third_index():
     names = ["Pedro","José","Maria","João"]
     print("3-", names[2])
     print("4-", names[3])
+
+def change_array_names():
+    """
+    Crie uma função que pede 3 nomes de alimentos digitado pelo usuário e substitui os elementos do array [“Macarrão”, “Pepino”, “Batata”] com esses 3 alimentos. Imprima o nome dos alimentos um abaixo do outro. Ao final faça um commit no GitHub
+    array_inicial = [“Macarrão”, “Pepino”, “Batata”]
+    Exemplo de entrada: Arroz, Feijão, Farinha
+    Exemplo de saída: 
+    1 - Arroz
+    2 - Feijão
+    3 - Farinha
+    """
+    array_inicial = ["Macarrão", "Pepino", "Batata"]
+
+    alimento1 = input("Digite o primeiro alimento: ")
+    alimento2 = input("Digite o segundo alimento: ")
+    alimento3 = input("Digite o terceiro alimento: ")
+
+    array_inicial[0] = alimento1
+    array_inicial[1] = alimento2
+    array_inicial[2] = alimento3
+
+    print("1-", array_inicial[0])
+    print("2-", array_inicial[1])
+    print("3-", array_inicial[2])
