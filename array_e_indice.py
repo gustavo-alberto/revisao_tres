ee517033369c614ada64dc994fc2a29bcac0f206
Gustavo Alberto <83519205+gustavo-alberto@users.noreply.github.com>
2024-04-04 20:26:47 -0300

    feat: Resolve questão 1

---
+++ array_e_indice.py
@@ -0,0 +1,14 @@
+def print_names():
+    """
+    1. Crie uma função que declara um array com 4 nomes diferentes e imprime cada um deles um embaixo do outro. Ao final faça um commit no GitHub.
+    Exemplo de entrada: Nenhuma Exemplo de saída:
+    1-João
+    2-Maria
+    3-Fulano
+    4-Ciclano
+    """
+    names = ["Pedro","José","Maria","João"]
+    print("1-", names[0])
+    print("2-", names[1])
+    print("3-", names[2])
+    print("4-", names[3])
