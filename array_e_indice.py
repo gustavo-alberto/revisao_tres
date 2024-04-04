2781caa9e975a8ddd9b46695cd000b4a19c9fc9c
Gustavo Alberto <83519205+gustavo-alberto@users.noreply.github.com>
2024-04-04 20:29:48 -0300

    feat: Resolve questão 2

--- array_e_indice.py
+++ array_e_indice.py
@@ -12,3 +12,15 @@ def print_names():
     print("2-", names[1])
     print("3-", names[2])
     print("4-", names[3])
+
+def print_first_last_name():
+    """
+    Crie uma função que declara um array com 4 nomes diferentes e imprime o primeiro e último nome do array. Ao final faça um commit no GitHub.
+    Exemplo de entrada : Nenhuma 
+    Exemplo de saída: 
+    1-João
+    4-Beltrano
+    """
+    names = ["Pedro","José","Maria","João"]
+    print("1-", names[0])
+    print("4-", names[3])
