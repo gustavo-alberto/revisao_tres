6e9b803afac3085c5dae6d6e257bbaed8cf2b98b
Gustavo Alberto <83519205+gustavo-alberto@users.noreply.github.com>
2024-04-08 13:56:32 -0300

    feat: Resolve questão 6

--- loops.py
+++ loops.py
@@ -10,3 +10,22 @@ def count_to_20():
     """
     for i in range(21):
         print(i)
+
+def count_to_n():
+    """
+    Crie uma função que imprime uma contagem de 0 até o número que o
+    usuário digitou. Ao final faça um commit no GitHub.
+    Exemplo de entrada: 3
+    Exemplo de saída:
+    0
+    1
+    2
+    3
+    """
+    try:
+        n = int(input("Deseja contar até que número? "))
+        for i in range(0, n+1):
+            print(i)
+    except:
+        print("Entrada inválida")
+        count_to_n()
