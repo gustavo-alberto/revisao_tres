c858b031dae40d9344775c08d7421fdec7a32a8e
Gustavo Alberto <83519205+gustavo-alberto@users.noreply.github.com>
2024-04-08 13:52:53 -0300

    feat: Resolve questão 5

---
+++ loops.py
@@ -0,0 +1,12 @@
+def count_to_20():
+    """
+    Crie uma função que imprime uma contagem de 0 a 20. Ao final faça um
+    commit no GitHub.
+    Exemplo de entrada: Nenhuma
+    Exemplo de saída:
+    0
+    1
+    2
+    """
+    for i in range(21):
+        print(i)
