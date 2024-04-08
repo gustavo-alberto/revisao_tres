b2d17697e1df17c3fc2212985bc343b10c5ffee5
Gustavo Alberto <83519205+gustavo-alberto@users.noreply.github.com>
2024-04-08 14:01:13 -0300

    feat: Resolve questão 7

--- loops.py
+++ loops.py
@@ -29,3 +29,18 @@ def count_to_n():
     except:
         print("Entrada inválida")
         count_to_n()
+
+def addition_table_2():
+    """
+    Crie uma função que imprime a tabuada de adição do número 2. Ao final
+    faça um commit no GitHub.
+    Entrada: Nenhuma
+    Exemplo de saída:
+    2 + 1 = 3
+    2 + 2 = 4
+    2 + 3 = 5
+    2 + 4 = 6
+    """
+    for i in range(1,11):
+        result = i + 2
+        print("2 + {} = {}".format(i, result))
